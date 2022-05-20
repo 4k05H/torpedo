902137c0c205dbe88d22e3cc71cb408319dde046
4K05H <rbenakos@gmail.con>
2022-05-20 12:33:29 +0200

    negyedik

---
+++ src/torpedo/Hajo.java
@@ -0,0 +1,13 @@
+package torpedo;
+
+public class Hajo {
+    
+    private int pozicio[3];
+    
+    public String talalat(int poz){
+
+        Hajo hajo = new Hajo;
+        new Hajo([2, 3, 4]);
+    
+    }    
+}
---
+++ src/torpedo/TorpedoTeszt.java
@@ -0,0 +1,17 @@
+package torpedo;
+    
+public class TorpedoTeszt {
+    
+    public static void main(String[] args) {
+
+        public String tesztLoves(int poz){
+
+                Hajo hajo = new Hajo();
+                String t = hajo.talalat(4);
+                assert t.equals("talált") : "nem jó a találat ellenőrzése";
+
+            }
+
+    }
+    
+}
